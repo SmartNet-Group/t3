d69f1d95ca252f67f7735a31c3eae0f85ca8b119
smartnet <ivan@kachinskiy.ru>
2017-11-10 20:29:55 +0300

    Сделали вручную пустой бандл SmartNET\SMetricBundle - для реализации модели в приложении

---
+++ src/SmartNET/SMetricBundle/SmartNETSMetricBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smartnet
+ * Date: 10.11.17
+ * Time: 20:23
+ */
+namespace SmartNET\SMetricBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SmartNETSMetricBundle extends Bundle
+{
+
+}
