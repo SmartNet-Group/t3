3c2a8479bb69fd270916f4bddfe4ba0fdb6a1e3a
Kachinskiy Ivan <ivan@kachinskiy.ru>
2017-11-16 13:06:44 +0300

    Полностью подготовлено для кодирования обработки запросов в БД

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace BlogBundle\Controller;
-require __DIR__.'/../../../src/SmartNET/SMetricBundle/InitApp.php';
+require __DIR__ . '/../../../src/SmartNET/SMetricBundle/InitAll.php';
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use function SmartNET\SMetricBundle\InitAll;
@@ -28,89 +28,51 @@ class DefaultController extends Controller
     {
         $result = $conn->query('SELECT * FROM users');
         $users = $result->fetchAll();
-
-        if (!isset($_SESSION['start'])) {
-            $_SESSION['start'] = true;
-            InitAll();
+        $action = "";
+        $params = [];
+        switch($_SERVER['REQUEST_METHOD'])
+        {
+            case 'GET':
+                if ($request->query->get('formName') == 'UserEdit') {
+                    $action = "Обрабатываем отредактированного пользователя";
+                }
+                if ($request->query->get('formName') == 'UserDelete') {
+                    $action = "Обрабатываем удаление пользователя";
+                }
+                if ($request->query->get('fomName') == 'NewUser') {
+                    $action = 'Обрабатываем создание нового пользователя';
+                }
+                $params = &$_GET;
+                break;
+            case 'POST':
+                if ($request->request->get('formName') == 'UserEdit') {
+                    $action = "Обрабатываем отредактированного пользователя";
+                }
+                if ($request->request->get('formName') == 'UserDelete') {
+                    $action = "Обрабатываем удаление пользователя";
+                }
+                if ($request->request->get('fomName') == 'NewUser') {
+                    $action = 'Обрабатываем создание нового пользователя';
+                }
+                $params = &$_GET;
+                break;
+            default:
         }
+//        if (!isset($_SESSION['start'])) {
+//            $_SESSION['start'] = true;
+//            InitAll();
+//        }
         return $this->render('@Blog/default/dblist.html.twig', [
-            'users' => $users
+            'users'     => $users,
+            'action'    => $action,
+            'params'    => $params
         ]);
     }
 
-    public function listAction(Request $request)
-    {
-//        global $uList;
-        $query = $_SERVER['QUERY_STRING'];
-        $act = "";
-//        $session = $request->getSession();
-//        if (!$session->has('start')) {
-//            $session->set('start', true);
-//        }
-
-        if (isset($_POST['btnSubmit'])) {
-            if ($_POST['btnSubmit'] == 'btnSave') {
-                // Сохраняем отредактированного пользователя в базу
-                $_SESSION['uList'][$_POST['uNumber']]->userName = $_POST['uName'];
-                $_SESSION['uList'][$_POST['uNumber']]->userEMail = $_POST['uEMail'];
-                $act = "Редактирование #".$_POST['uNumber'];
-            } elseif ($_POST['btnSubmit'] == 'btnNotSave') {
-                $act = "Оставляем 'как есть' #".$_POST['uNumber'];
-                // Ничего не сохраняем и переходим к списку
-            } elseif ($_POST['btnSubmit'] == 'btnDelete') {
-                // удаляем заданного пользователя
-                unset($_SESSION['uList'][$_POST['uNumber']]);
-                $act = "Удаление #".$_POST['uNumber'];
-            } elseif ($_POST['btnSubmit'] == 'btnNotDelete') {
-                // Ничего не удаляем и переходим к списку
-                $act = "НЕ удаляем #".$_POST['uNumber'];
-            } elseif ($_POST['btnSubmit'] == 'SaveNew') {
-                // Сохраняем нового пользователя
-                $newUser = new Users\User($_POST['uName'], $_POST['uEMail'], $_POST['uPass']);
-                $newUser->Add();
-                $act = "Сохраняем нового пользователя";
-            } elseif ($_POST['btnSubmit'] == 'DoNothing') {
-                //  Ничего не делаем и переходим к списку
-                $act = "Ничего не делаем!";
-            }
-        }
-        if (isset($_SESSION['uList'])) {
-            $uNum = count($_SESSION['uList']);
-            return $this->render('@Blog/default/list.html.twig', [
-                'uList' => $_SESSION['uList'],
-                'uNum'  => $uNum,
-                'query' => $query,
-                'action' => $act,
-                'start' => $_SESSION['start']
-            ]);
-        } else {
-            $uNum = 0;
-            return $this->render('@Blog/default/list.html.twig', [
-                'uList' => null,
-                'uNum'  => $uNum,
-                'query' => $query,
-                'action' => $act,
-                'start' => $_SESSION['start']
-            ]);
-        }
-    }
 
     public function newAction(Request $request, Connection $conn)
     {
         return $this->render('@Blog/default/new.html.twig', [
-        ]);
-    }
-
-    public function editAction(Request $request, $uN)
-    {
-        $uName = $_SESSION['uList'][$uN]->userName;
-        $uEMail = $_SESSION['uList'][$uN]->userEMail;
-        $uPass = $_SESSION['uList'][$uN]->userPass;
-        return $this->render('@Blog/default/edit.html.twig', [
-            'uN'    => $uN,
-            'uName' =>  $uName,
-            'uEMail' => $uEMail,
-            'uPass' => $uPass
         ]);
     }
 
@@ -131,6 +93,7 @@ class DefaultController extends Controller
             'uPass' => $uPass
         ]);
     }
+
     public function deleteAction(Request $request, $uN, Connection $conn)
     {
         $result = $conn->query('SELECT * FROM users WHERE id='.$uN);
